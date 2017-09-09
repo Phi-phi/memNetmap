0157be92c1cdb1068f926aed7a1b39adc25c7524
Kazuaki Masuda <kame.kazu.0213.masumerry.0112@gmail.com>
2017-09-09 21:31:40 +0900

    watch only first rxring

--- sample/sample1.c
+++ sample/sample1.c
@@ -19,7 +19,7 @@ void printHex(char* buf) {
 }
 
 int main(int argc, char* argv[]) {
-  unsigned int cur, n, i;
+  unsigned int cur, i;
   char *buf, *payload;
   struct netmap_ring *rxring;
   struct pollfd pollfd[1];
@@ -37,37 +37,34 @@ int main(int argc, char* argv[]) {
     pollfd[0].events = POLLIN;
     poll(pollfd, 1, 100);
 
-    for(i = nm_desc->first_rx_ring; i <= nm_desc->last_rx_ring; ++i){
+    rxring = NETMAP_RXRING(nm_desc->nifp, nm_desc->first_rx_ring);
 
-      rxring = NETMAP_RXRING(nm_desc->nifp, i);
+    while(!nm_ring_empty(rxring)) {
+      cur = rxring->cur;
+      buf = NETMAP_BUF(rxring, rxring->slot[cur].buf_idx);
+      printHex(buf);
+      ether = (struct ether_header *)buf;
+      ip = (struct ip *)(buf + sizeof(struct ether_header));
+      printf("ip: ");
+      printfHex((char*)ip);
+      payload = (char *)(ip + ip->ip_hl * 4);
 
-      while(!nm_ring_empty(rxring)) {
-        cur = rxring->cur;
-        buf = NETMAP_BUF(rxring, rxring->slot[cur].buf_idx);
-        printHex(buf);
-        ether = (struct ether_header *)buf;
-        ip = (struct ip *)(buf + sizeof(struct ether_header));
-        printf("ip: ");
-        printfHex((char*)ip);
-        payload = (char *)(ip + ip->ip_hl * 4);
+      inet_ntop(AF_INET, &ip->ip_src, src, sizeof(src));
+      inet_ntop(AF_INET, &ip->ip_dst, dst, sizeof(dst));
 
-        inet_ntop(AF_INET, &ip->ip_src, src, sizeof(src));
-        inet_ntop(AF_INET, &ip->ip_dst, dst, sizeof(dst));
-
-        if (ip->ip_p == IPPROTO_TCP) {
-          tcp = (struct tcphdr *)payload;
-          printf("TCP Src port: %d\n", ntohs(tcp->th_sport));
-          printf("TCP Dst port: %d\n", ntohs(tcp->th_dport));
-        } else if (ip->ip_p == IPPROTO_UDP) {
-          udp = (struct udphdr *)payload;
-          printf("UDP Src port: %d\n", ntohs(udp->uh_sport));
-          printf("UDP Dst port: %d\n", ntohs(udp->uh_dport));
-        }
-
-        printf("payload: ");
-        printHex(payload);
-        rxring->head = rxring->cur = nm_ring_next(rxring, cur);
+      if (ip->ip_p == IPPROTO_TCP) {
+        tcp = (struct tcphdr *)payload;
+        printf("TCP Src port: %d\n", ntohs(tcp->th_sport));
+        printf("TCP Dst port: %d\n", ntohs(tcp->th_dport));
+      } else if (ip->ip_p == IPPROTO_UDP) {
+        udp = (struct udphdr *)payload;
+        printf("UDP Src port: %d\n", ntohs(udp->uh_sport));
+        printf("UDP Dst port: %d\n", ntohs(udp->uh_dport));
       }
+
+      printf("payload: ");
+      printHex(payload);
+      rxring->head = rxring->cur = nm_ring_next(rxring, cur);
     }
 
     if (ioctl(nm_desc->fd, NIOCRXSYNC, NULL) != 0)
