16afa076eb59e6b23fd618c309e4935977bbde84
Kazuaki Masuda <kame.kazu.0213.masumerry.0112@gmail.com>
2017-09-13 16:57:54 +0900

    big change send sample

--- sample/send_packet.c
+++ sample/send_packet.c
@@ -10,48 +10,96 @@
 
 struct nm_desc *nm_desc;
 
+void printHex(char* buf, size_t len) {
+  int i;
+  for(i = 0; i < len; ++i) {
+    printf("%02X", ((unsigned char*)buf)[i]);
+  }
+  printf("\n");
+}
+
+void create_iphdr(char* buf, struct in_addr *src, struct in_addr *dst, size_t len) {
+  struct ip *ip;
+
+  ip = (struct ip *)buf;
+  ip->ip_v = 4;
+  ip->ip_hl = 5;
+  ip->ip_tos = 1;
+  ip->ip_len = len;
+  ip->ip_id = htons(213);
+  ip->ip_off = 0;
+  ip->ip_ttl = 0x40;
+  ip->ip_p = IPPROTO_UDP;
+  ip->ip_src = *src;
+  ip->ip_dst = *dst;
+  ip->ip_sum = 0;
+}
+
+void create_udphdr(char* buf, unsigned short dport, char* data) {
+  char* u_buf;
+  struct ip *ip;
+  struct udphdr *udp;
+
+  udp = (struct udphdr *)u_buf;
+  udp->uh_sport = htons(65001);
+  udp->uh_dport = htons(dport);
+  udp->uh_ulen = htons(sizeof(struct udphdr) + strlen(data));
+  udp->uh_sum = 0;
+
+  ip = (struct ip *)buf;
+  memcpy(buf + (ip->ip_hl << 2), udp, udp->uh_ulen);
+}
+
 int main(int argc, char* argv[]) {
-  unsigned int rx_cur, tx_cur, n, i;
-  char *rbuf, *tbuf, *payload;
-  struct netmap_ring *rxring, *txring;
+  int sent = 0, pktsizelen;
+  unsigned int cur, n, i;
+  char *pkt, *tbuf, *data, *counted_data;
+  struct in_addr src, dst;
+  struct netmap_ring *txring;
   struct pollfd pollfd[1];
   struct ether_header *ether;
   struct ip *ip;
-  struct tcphdr *udp;
+  struct udphdr *udp;
 
-  nm_desc = nm_open("netmap:ix1*", NULL, 0, NULL);
+  data = argv[1];
+  src.s_addr = inet_addr("10.2.2.3");
+  dst.s_addr = inet_addr("10.2.2.3");
+
+  nm_desc = nm_open("netmap:ix1", NULL, 0, NULL);
   for(;;){
     pollfd[0].fd = nm_desc->fd;
-    pollfd[0].events = POLLIN;
+    pollfd[0].events = POLLOUT;
     poll(pollfd, 1, 500);
 
-    rxring = NETMAP_RXRING(nm_desc->nifp, nm_desc->first_rx_ring);
-    txring = NETMAP_TXRING(nm_desc->nifp, nm_desc->first_tx_ring);
+    if(!sent) {
+      txring = NETMAP_TXRING(nm_desc->nifp, nm_desc->first_tx_ring);
+      cur = txring->cur;
 
-    while(!nm_ring_empty(rxring)) {
-      struct in_addr addr;
-      rx_cur = rxring->cur;
-      tx_cur = txring->cur;
+      for(i = 0; i < 30 || nm_ring_empty(txring); ++i) {
+        cur = txring->cur;
+        tbuf = NETMAP_BUF(txring, txring->slot[cur].buf_idx);
+        sprintf(counted_data, "%s%d", data, i);
 
-      rbuf = NETMAP_BUF(rxring, rxring->slot[rx_cur].buf_idx);
-      tbuf = NETMAP_BUF(txring, txring->slot[tx_cur].buf_idx);
-      printf("packet: %x\n", *rbuf);
+        printHex(tbuf, txring->slot[n].len);
 
-      ether = (struct ether_header *)rbuf;
-      ip = (struct ip *)(rbuf + sizeof(struct ether_header));
+        pktsizelen = sizeof(struct ip) + sizeof(struct udphdr) + strlen(counted_data);
 
-      addr = ip->ip_src;
-      ip->ip_src = ip->ip_dst;
-      ip->ip_dst = addr;
-      printf("modified %x\n", *rbuf);
+        pkt = malloc(pktsizelen);
 
-      memcpy(tbuf, rbuf, sizeof(*rbuf));
-      txring->slot[tx_cur].len = sizeof(*rbuf);
-      txring->slot[tx_cur].flags |= NS_BUF_CHANGED;
-      rxring->head = rxring->cur = nm_ring_next(rxring, rx_cur);
-      txring->cur = nm_ring_next(txring, tx_cur);
+        if(pkt == NULL)
+          perror("malloc");
+
+        create_iphdr(pkt, &src, &dst, pktsizelen);
+        create_udphdr(pkt, 12345, counted_data);
+
+        printHex(pkt, pktsizelen);
+        nm_pkt_copy(pkt, tbuf, pktsizelen);
+        txring->slot[cur].flags |= NS_BUF_CHANGED;
+
+        txring->head = txring->cur = nm_ring_next(txring, cur);
+        free(pkt);
+      }
+      sent = 1;
     }
-
-
   }
 }
