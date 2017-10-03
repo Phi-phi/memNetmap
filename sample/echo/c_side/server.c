02faa3af00082bce3773e73d91e3f9850769c561
Kazuaki Masuda <kame.kazu.0213.masumerry.0112@gmail.com>
2017-10-03 22:30:43 +0900

    fix send program

--- sample/echo/c_side/server.c
+++ sample/echo/c_side/server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -9,30 +10,26 @@
 int main() {
   int ld;
   struct sockaddr_in skaddr, remote;
-  int length;
   int len,n;
   char bufin[512];
 
-  if ((ld = socket( PF_INET, SOCK_DGRAM, 0 )) < 0) {
+  if ((ld = socket( PF_INET, SOCK_DGRAM, IPPROTO_UDP )) < 0) {
     printf("Problem creating socket\n");
     exit(1);
   }
 
+  memset(&skaddr, 0, sizeof(skaddr));
   skaddr.sin_family = AF_INET;
   skaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  skaddr.sin_port = htons(0);
+  skaddr.sin_port = htons(11233);
 
   if (bind(ld, (struct sockaddr *) &skaddr, sizeof(skaddr))<0) {
     printf("Problem binding\n");
     exit(0);
   }
 
-  length = sizeof( skaddr );
-  if (getsockname(ld, (struct sockaddr *) &skaddr, &length)<0) {
-    printf("Error getsockname\n");
-    exit(1);
-  }
-
+  memset(&remote, 0, sizeof(remote));
+  
   while (1) {
     n = recvfrom(ld, bufin, 512, 0, (struct sockaddr *)&remote, &len);
 
